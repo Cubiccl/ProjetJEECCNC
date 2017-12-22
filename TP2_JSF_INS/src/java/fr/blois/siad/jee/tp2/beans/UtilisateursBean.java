9896ea39cd16159d93ca84c83c077f87667d01e8
CANDELA Nicolas <darkxell.mc@gmail.com>
2017-12-22 19:04:22 +0100

    Plein de trucs

--- TP2_JSF_INS/src/java/fr/blois/siad/jee/tp2/beans/UtilisateursBean.java
+++ TP2_JSF_INS/src/java/fr/blois/siad/jee/tp2/beans/UtilisateursBean.java
@@ -1,9 +1,11 @@
 package fr.blois.siad.jee.tp2.beans;
 
-import fr.blois.siad.jee.tp2.dto.Utilisateur;
-import fr.blois.siad.jee.tp2.services.UtilisateurService;
+import fr.blois.siad.jee.tp3.entities.UtilisateurEntity;
+import fr.blois.siad.jee.tp3.entities.dto.Utilisateur;
+import fr.blois.siad.jee.tp3.services.UtilisateurService;
 import java.util.Date;
 import java.util.List;
+import java.util.function.Predicate;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
@@ -65,26 +67,66 @@ public class UtilisateursBean {
     public void setUtilisateurs(List<Utilisateur> utilisateurs) {
 
     }
+    
+    public String changerMdp() {
+        List<Utilisateur> users = getService().listerTous();
+        users.removeIf(new Predicate<Utilisateur>(){
+            @Override
+            public boolean test(Utilisateur t) {
+                return !t.getNom().equals(getNom());
+            }
+        });
+        if (users.size() == 0) {
+            FacesContext context = FacesContext.getCurrentInstance();
+            context.addMessage(null, new FacesMessage("Utilisateur inconnu!! >:["));
+            return null;
+        } else {
+            users.get(0).setMotDePasse(getMotDePasse());
+            return "index";
+        }
+    }
 
     public String checkAddUser() {
+        
+        List<Utilisateur> users = getService().listerTous();
 
         // Validation
         FacesContext context = FacesContext.getCurrentInstance();
         if (email == null || email.isEmpty()) {
             context.addMessage(null, new FacesMessage("Email obligatoire"));
+        } else if (!email.matches("([A-z]|\\.|[0-9])*@[A-z]*\\.(com|fr|net)")) {
+            context.addMessage(null, new FacesMessage("Ton email est faux petit galopin."));
+        } else {
+            boolean found = false;
+            for (Utilisateur u : users)
+                if (u.getEmail().equals(email)){
+                    found=true;
+                    break;
+                }
+            if (found) context.addMessage(null, new FacesMessage("IMPOSTEUR quelqu'un a déjà ton email."));
         }
         if (motDePasse == null || motDePasse.isEmpty()) {
             context.addMessage(null, new FacesMessage("MDP obligatoire"));
+        } else if (motDePasse.length() < 8) {
+            context.addMessage(null, new FacesMessage("Ton mot de passe est trop petit."));
         }
         if (nom == null || nom.isEmpty()) {
             context.addMessage(null, new FacesMessage("Nom obligatoire"));
+        } else {
+            boolean found = false;
+            for (Utilisateur u : users)
+                if (u.getNom().equals(nom)){
+                    found=true;
+                    break;
+                }
+            if (found) context.addMessage(null, new FacesMessage("IMPOSTEUR quelqu'un a déjà ton nom."));
         }
         if (!context.getMessageList().isEmpty()) {
             return null;
         }
 
         // Cas nominal
-        getService().ajouter(new Utilisateur(null, email, motDePasse, nom, new Date()));
+        getService().ajouter(new UtilisateurEntity(email, motDePasse, nom, new Date()));
         return "index";
     }
     
